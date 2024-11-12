1b4d8daeaf1d288f96bf32dd7a9b0d7d4934d457
Alexander Ryzhakov <aryzhakov87@gmail.com>
2024-11-12 13:15:10 +1000

    Rework1

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,6 +1,6 @@
 public class BmiService {
-    public int calculate(double height_meters, double weight_kg){
-        double index = weight_kg / (height_meters * height_meters);
-        return (int)index;
+    public int calculate(double heightMeters, double weightKg) {
+        double index = weightKg / (heightMeters * heightMeters);
+        return (int) index;
     }
 }
